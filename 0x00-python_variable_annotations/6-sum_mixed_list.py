469b68d03a3b5d02d35f1bc4b11c030fa1f9a943
SeM2x <houssem135.302@gmail.com>
2024-10-10 12:14:23 +0100

    checkers fix

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """function that returns the sum of the list of floats"""
     return sum(mxd_lst)
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,7 +4,9 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """function that returns a function that multiplies a float by multiplier"""
+    """
+    function that returns a function that multiplies a float by multiplier
+    """
     def multiply(n: float) -> float:
         """function that multiplies a float by multiplier"""
         return multiplier * n
