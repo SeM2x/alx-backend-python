dc9c6fcf50c926b6273f299b184217f3075b27d5
SeM2x <houssem135.302@gmail.com>
2024-10-14 19:36:55 +0100

    0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-"""module that defines wait_random function"""
+'''module that defines wait_random function
+'''
 import asyncio
-from random import random
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Wait for a random delay between 0 and `max_delay` seconds."""
-    delay: float = random() * max_delay
+    '''Wait for a random delay between 0 and `max_delay` seconds
+    '''
+    delay = random.random() * max_delay
     await asyncio.sleep(delay)
     return delay
-
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random(5)))
-print(asyncio.run(wait_random(15)))
