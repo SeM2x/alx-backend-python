84c00b3eb1ef327eb2df3a798bc06c66e737cdb3
SeM2x <houssem135.302@gmail.com>
2024-10-14 20:20:15 +0100

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 '''module that defines wait_random function
 '''
-import asyncio
-
-
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''Wait for a random delay between 0 and `max_delay` seconds
     '''
     delays = [await wait_random(max_delay) for i in range(n)]
